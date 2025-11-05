8a901bc8fc6e3097b9977f7275024bf62ef05e7e
JPLDN <149190497+JPLDN@users.noreply.github.com>
2025-11-05 16:51:24 +0000

    Starting the main player character movement animation, imported Andy Melon sprite into Unity

--- Assets/Scripts/Player/TopDownMovement.cs
+++ Assets/Scripts/Player/TopDownMovement.cs
@@ -7,10 +7,14 @@ public class TopDownMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     public Vector2 movement;
+
+    [SerializeField] private Animator anim;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
     }
 
@@ -18,6 +22,16 @@ public class TopDownMovement : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = movement * moveSpeed;
+
+        if (anim != null)
+        {
+            anim.SetFloat("Horizontal", movement.x);
+            anim.SetFloat("Vertical", movement.y);
+            anim.SetFloat("Speed", movement.sqrMagnitude);
+        }
+
+        if (spriteRenderer != null)
+            spriteRenderer.flipX = movement.x < 0;
     }
 
     public void Move(InputAction.CallbackContext context)
